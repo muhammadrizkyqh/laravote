de4d53962874f1164d45a0211d2e6a84ae2d61d6
Aliciaindira <aliciaindira06@gmail.com>
2024-06-13 15:46:27 +0700

    Penyesuaian Pada Direktori App

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,38 +7,44 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+   
     protected $fillable = [
         'name',
         'email',
         'password',
+        'token',
+        'kelas_id',
+        'kandidat_id'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+    
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+   
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $appends = ['role_name'];
+
+    public function getRoleNameAttribute()
+    {
+        return $this->roles->pluck('name')[0];
+    }
+
+    public function Kelas()
+    {
+        return $this->belongsTo(Kelas::class,'kelas_id');
+    }
+
+    
 }
